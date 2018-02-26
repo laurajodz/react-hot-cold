513d603de7e398dbcb523a405fe5dab79cb38961
Laura Jodz <laurajodz@gmail.com>
2018-02-26 00:10:33 -0800

    add redux

--- src/components/top-nav.js
+++ src/components/top-nav.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {connect} from 'react-redux';
+
+import {generateAuralUpdate, restartGame} from '../actions';
 
 import './top-nav.css';
 
@@ -7,8 +10,8 @@ export default function TopNav(props) {
     <nav>
       <ul className="clearfix">
         <li>
-          <a 
-            href="#what" 
+          <a
+            href="#what"
             className="what"
             aria-label="How to play"
           >
@@ -20,19 +23,23 @@ export default function TopNav(props) {
             href="#feedback"
             className="new"
             aria-label="Start a new game"
-            onClick={() => props.onRestartGame()}
-          >
+            onClick={() =>
+              props.dispatch(
+                restartGame(Math.floor(Math.random() * 100) + 1)
+              )}>
             + New Game
           </a>
         </li>
         <li>
           <a
             href="#get-status"
-            /* the `visuallyhidden` class hides an element 
+            /* the `visuallyhidden` class hides an element
             while leaving it available to screen reader users  */
             className="visuallyhidden focusable status-link"
-            onClick={() => props.onGenerateAuralUpdate()}
-          >
+            onClick={() =>
+              props.dispatch(
+                generateAuralUpdate()
+              )}>
             Hear state of game
           </a>
         </li>
