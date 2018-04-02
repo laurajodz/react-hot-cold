f435c5005988cd6afd94772ca9e20e9a53c483bb
Laura Jodz <laurajodz@gmail.com>
2018-04-02 09:57:57 -0700

    final

--- src/components/info-section.js
+++ src/components/info-section.js
@@ -12,7 +12,7 @@ export default function InfoSection(props) {
         <li>You need to <strong>guess</strong> until you can find the hidden secret number.</li>
         <li>You will <strong>get feedback</strong> on how close ("hot") or far ("cold") your guess is.</li>
       </ol>
-      <p>If you're using a screen reader, you can use the visually hidden "Hear state of game" link in the navigation region to get a reminder of all your past guesses and how close your current guess is to being correct.</p>
+      <p>If you are using a screen reader, you can use the visually hidden "Hear state of game" link in the navigation region to get a reminder of all your past guesses and how close your current guess is to being correct.</p>
     </section>
   );
 }
